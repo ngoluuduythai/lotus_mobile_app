745c21055c874a33196c3a161fc1f0f54d09170b
Brian Schardt <brianschardt@usnyc038358.music.wmgis.com>
2019-10-10 17:00:46 -0400

    added google login for ios

--- lib/widgets/facebook_button/facebook_button.dart
+++ lib/widgets/facebook_button/facebook_button.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:main/services/facebook.service.dart';
 import 'package:main/locator.dart';
 import 'package:main/store/auth_user/auth_user.store.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class FacebookButton extends StatelessWidget {
   final authUserStore = locator<AuthUserStore>();
+  var _googleSignIn = GoogleSignIn();
 
   final Function afterLogin;
   FacebookButton({@required this.afterLogin});
 
   onPressed() async {
     await authUserStore.login();
+    // print("signin with google");
+    // var res = await _googleSignIn.signIn();
+    // print("ress");
+    // // print(res);
+    // var other = await _googleSignIn.currentUser.authentication;
+    // print(other.idToken);
     this.afterLogin();
   }
   
