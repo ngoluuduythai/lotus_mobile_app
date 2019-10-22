2cd841bc310b847adb0a79a95f0f7ecf8f8a200b
Webhst <pbee.solutions@gmail.com>
2019-10-22 21:42:06 +0200

    Architecture to responsive

---
+++ lib/views/home_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:main/responsive/orientatio_layout.dart';
+import 'package:main/responsive/screen_type_layouts.dart';
+import 'package:main/views/home_view_tablet.dart';
+
+import 'home_view_mobile.dart';
+
+class HomeView extends StatelessWidget {
+  HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout(
+      mobile: OrientationLayout(
+        portrait:HomeMobilePortrait(),
+        landscape: HomeMobileLandscape(),
+      ),
+      tablet: HomeViewTablet(),
+    );
+  }
+}
---
+++ lib/views/home_view_mobile.dart
@@ -0,0 +1,46 @@
+// Contains the widgets that will be used for Mobile layout of Home , 
+// portrait and landscape
+
+import 'package:flutter/material.dart';
+import 'package:main/widgets/app_drawer/app_drawer.dart';
+
+class HomeMobilePortrait  extends StatelessWidget {
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  HomeMobilePortrait({Key key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: AppDrawer(),
+      key: _scaffoldKey,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child : IconButton(
+              icon: Icon(Icons.menu,size : 30),
+              onPressed: (){
+                _scaffoldKey.currentState.openDrawer();
+              },
+            )
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class HomeMobileLandscape extends StatelessWidget {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+    HomeMobileLandscape({Key key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(children: <Widget>[
+        AppDrawer()
+      ],),
+    );
+  }
+}
