b9256210d11abb80232a44326f2314dd22b5837e
Matias Zelaya <malday>
2019-11-05 21:43:37 -0300

    Change to remove the time to load

--- lib/routes/splash_screen/splash_screen.dart
+++ lib/routes/splash_screen/splash_screen.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import '../../routes.dart';
 
+
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
-
 class _SplashScreenState extends State<SplashScreen> {
   Timer _timer;
   @override
@@ -14,32 +14,29 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     _timer = Timer(const Duration(milliseconds: 500), _onShowLogin);
   }
-
   @override
   void dispose() {
     _timer.cancel();
     super.dispose();
   }
-
   void _onShowLogin() {
     if (mounted) {
       Routes.sailor.navigate('/login');
     }
   }
-
-  @override
+@override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.white,
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Image.asset(
-          'assets/images/logo.png',
-          width: 112.0,
-          height: 68.0,
+    alignment: Alignment.center,
+    color: Colors.white,
+    child: Container(
+      width: MediaQuery.of(context).size.width,
+      child: Image.asset(
+          'assets/images/logo.png', width: 112.0, height: 68.0,
         ),
-      ),
-    );
+    ),
+  );
   }
 }
+
+ 
