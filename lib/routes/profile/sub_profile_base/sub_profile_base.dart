2db99ffd26bd8e17d91682d677d180ebfadb60f0
Webhst <pbee.solutions@gmail.com>
2019-12-04 20:29:57 -0300

    UIChores

--- lib/routes/profile/sub_profile_base/sub_profile_base.dart
+++ lib/routes/profile/sub_profile_base/sub_profile_base.dart
@@ -16,50 +16,55 @@ class SubProfileBase extends StatelessWidget {
         backgroundColor: Color.fromRGBO(245, 246, 250, 1),
         body: ListView(
           children: <Widget>[
-            Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(left: 5, right: 20, top: 30),
-                  child: Row(
-                    children: <Widget>[
-                      IconButton(
-                        icon: Image.asset(
-                          IconPath.backArrow,
-                          width: ScreenUtil().setWidth(38),
-                          height: ScreenUtil().setWidth(34),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.only(
+                        left: 0, right: ScreenUtil().setWidth(120), top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        IconButton(
+                          icon: Image.asset(
+                            IconPath.backArrow,
+                            width: ScreenUtil().setWidth(38),
+                            height: ScreenUtil().setWidth(34),
+                          ),
+                          onPressed: () {
+                            Routes.sailor.navigate(
+                              RouteNames.profile,
+                            );
+                          },
                         ),
-                        onPressed: () {
-                          Routes.sailor.navigate(
-                            RouteNames.profile,
-                          );
-                        },
-                      ),
-                      Container(
-                          margin: EdgeInsets.only(),
-                          child: GestureDetector(
-                            child: Text(
-                              name,
-                              style: TextStyle(
-                                color: Color(0xff0b0b0b),
-                                fontWeight: FontWeight.w700,
-                                fontFamily: 'AirbnbCerealApp',
-                                fontStyle: FontStyle.normal,
-                                fontSize: ScreenUtil().setSp(42.0),
+                        Container(
+                            margin: EdgeInsets.only(),
+                            child: GestureDetector(
+                              child: Text(
+                                name,
+                                style: TextStyle(
+                                  color: Color(0xff0b0b0b),
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'AirbnbCerealApp',
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: ScreenUtil().setSp(42.0),
+                                ),
                               ),
-                            ),
-                            onTap: () =>
-                                Routes.sailor.navigate(RouteNames.profile),
-                          )),
-                      Container(
-                        margin: EdgeInsets.only(
-                            left: ScreenUtil()
-                                .setWidth(ScreenUtil().setWidth(610))),
-                        child: save,
-                      )
-                    ],
+                              onTap: () =>
+                                  Routes.sailor.navigate(RouteNames.profile),
+                            )),
+                        Expanded(
+                            child: Container(
+                          margin: EdgeInsets.only(
+                              left: ScreenUtil()
+                                  .setWidth(ScreenUtil().setWidth(510))),
+                          child: save,
+                        )),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
             child,
           ],
--- lib/routes/profile/sub_routes/termofservice/termofservice.route.dart
+++ lib/routes/profile/sub_routes/termofservice/termofservice.route.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:main/routes/profile/sub_profile_base/sub_profile_base.dart';
+import 'package:main/shared/widgets/base_widget/base_widget.dart';
+import 'package:main/shared/widgets/bottom_navigation_base/bottom_navigation_base.dart';
 
 class TermofserviceRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text("Hello World"),
-        ],
-      ),
-    ));
+    return BaseWidget(builder: (context, sizingInformation) {
+      return BottomNavigationBase(
+          child: SubProfileBase(
+        name: 'Term of Service',
+        child: Container(),
+      ));
+    });
   }
 }
